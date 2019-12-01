92c0d14e3d560d1e1404e4083d3642a3a921377b
Aurelius Wendelken <webtobesocial@gmail.com>
2019-12-01 21:45:03 +0100

    implemented vuex persistedstate plugin to store in localstorage

---
+++ plugins/localStorage.js
@@ -0,0 +1,12 @@
+import createPersistedState from 'vuex-persistedstate'
+
+export default ({
+  store
+}) => {
+  window.onNuxtReady(() => {
+    createPersistedState({
+      key: 'vuex',
+      paths: [],
+    })(store)
+  })
+}
