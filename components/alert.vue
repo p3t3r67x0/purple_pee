ed86d4ad3e5e74a15220de2003ca92354f10533e
Aurelius Wendelken <webtobesocial@gmail.com>
2019-12-03 09:55:50 +0100

    created alert component without any function

---
+++ components/alert.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="container mx-auto">
+  <div class="bg-white rounded w-full shadow-md leading-normal mb-6 p-3">
+    <div class="flex bg-green-100 p-4 mb-3">
+      <div class="mr-4">
+        <div class="h-10 w-10 text-white bg-green-700 rounded-full flex justify-center items-center"></div>
+      </div>
+      <div class="flex justify-between w-full">
+        <div class="text-green-700">
+          <p class="mb-2 font-bold">Succes alert</p>
+          <p class="text-xs">description text</p>
+        </div>
+      </div>
+    </div>
+    <div class="flex bg-blue-100 p-4 mb-3">
+      <div class="mr-4">
+        <div class="h-10 w-10 text-white bg-blue-700 rounded-full flex justify-center items-center"></div>
+      </div>
+      <div class="flex justify-between w-full">
+        <div class="text-blue-700">
+          <p class="mb-2 font-bold">Info alert</p>
+          <p class="text-xs">description text</p>
+        </div>
+      </div>
+    </div>
+    <div class="flex bg-orange-100 p-4 mb-3">
+      <div class="mr-4">
+        <div class="h-10 w-10 text-white bg-orange-700 rounded-full flex justify-center items-center"></div>
+      </div>
+      <div class="flex justify-between w-full">
+        <div class="text-orange-700">
+          <p class="mb-2 font-bold">Warning alert</p>
+          <p class="text-xs">description text</p>
+        </div>
+      </div>
+    </div>
+    <div class="flex bg-red-100 p-4">
+      <div class="mr-4">
+        <div class="h-10 w-10 text-white bg-red-700 rounded-full flex justify-center items-center"></div>
+      </div>
+      <div class="flex justify-between w-full">
+        <div class="text-red-700">
+          <p class="mb-2 font-bold">Danger alert</p>
+          <p class="text-xs">description text</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
