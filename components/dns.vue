bc4ae2a02619d1d0dd7330aa05a10e38277788b3
Aurelius Wendelken <webtobesocial@gmail.com>
2019-12-03 13:29:05 +0100

    display ssl certificate information for each entry when available

--- components/dns.vue
+++ components/dns.vue
@@ -1,10 +1,10 @@
 <template>
 <div class="container mx-auto">
   <div v-for="result in results" class="overflow-hidden bg-white rounded w-full shadow-md leading-normal mb-6 p-3">
-    <div v-if="result.domain" class="mb-4 text-3xl font-medium text-purple-700">
+    <div v-if="result.domain" class="text-3xl font-medium text-purple-700">
       {{ result.domain }}
     </div>
-    <div v-if="result.a_record" class="mb-4">
+    <div v-if="result.a_record" class="mt-4">
       <strong>A redords</strong>
       <ul class="text-xl text-gray-700 font-light">
         <li v-for="a_record in result.a_record">
@@ -12,7 +12,7 @@
         </li>
       </ul>
     </div>
-    <div v-if="result.aaaa_record" class="mb-4">
+    <div v-if="result.aaaa_record" class="mt-4">
       <strong>AAAA redords</strong>
       <ul class="text-xl text-gray-700 font-light">
         <li v-for="aaaa_record in result.aaaa_record">
@@ -20,7 +20,7 @@
         </li>
       </ul>
     </div>
-    <div v-if="result.cname_record" class="mb-4">
+    <div v-if="result.cname_record" class="mt-4">
       <strong>CNAME redords</strong>
       <ul class="text-xl text-gray-700 font-light">
         <li v-for="cname_record in result.cname_record">
@@ -28,7 +28,7 @@
         </li>
       </ul>
     </div>
-    <div v-if="result.mx_record" class="mb-4">
+    <div v-if="result.mx_record" class="mt-4">
       <strong>MX redords</strong>
       <ul class="text-xl text-gray-700 font-light">
         <li v-for="mx_record in result.mx_record">
@@ -36,18 +36,48 @@
         </li>
       </ul>
     </div>
-    <div v-if="result.banner" class="mb-4">
+    <div v-if="result.banner" class="mt-4">
       <strong>SSH banner</strong>
       <ul class="text-xl text-gray-700 font-light">
         {{ result.banner }}
       </ul>
     </div>
-    <div v-if="result.header">
+    <div v-if="result.ssl_cert" class="mt-4">
+      <strong>SSL cert</strong>
+      <ul v-for="ssl_cert in result.ssl_cert" class="font-mono overflow-scroll text-md font-light">
+        <li v-for="val, key in ssl_cert" class="mb-1">
+          <div v-if="key == 'subject'">
+            <div v-for="v, k in val">
+              <span class="font-bold">Subject {{ k }}</span>: <span class="text-gray-700 font-thin">{{ v }}</span>
+            </div>
+          </div>
+          <div v-if="key == 'issuer'">
+            <div v-for="v, k in val">
+              <span class="font-bold">Issuer {{ k }}</span>: <span class="text-gray-700 font-thin">{{ v }}</span>
+            </div>
+          </div>
+          <div v-if="key == 'hash'">
+            <div v-for="v, k in val">
+              <span class="font-bold">{{ k }}</span>: <span class="text-gray-700 font-thin">{{ v }}</span>
+            </div>
+          </div>
+          <div v-if="key == 'extensions'">
+            <div v-for="v, k in val">
+              <span class="font-bold">{{ k }}</span>: <span class="text-gray-700 font-thin">{{ v }}</span>
+            </div>
+          </div>
+          <div v-if="key != 'cert' && key != 'extensions' && key != 'hash' && key != 'issuer' && key != 'subject'">
+            <span class="font-bold">{{ key }}</span>: <span class="text-gray-700 font-thin">{{ val }}</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div v-if="result.header" class="mt-4">
       <strong>HTTP header</strong>
       <code>
         <ul class="bg-gray-300 overflow-scroll px-3 pt-3">
           <li v-for="val, key in result.header">
-            <span class="font-bold">{{ key }}</span>: <span class="">{{ val }}</span>
+            <span class="font-bold">{{ key }}</span>: <span class="font-thin">{{ val }}</span>
           </li>
         </ul>
       </code>
