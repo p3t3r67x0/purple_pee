754549bfebe8b1327fba6ed38560630aea22120a
Aurelius Wendelken <webtobesocial@gmail.com>
2019-12-03 04:59:03 +0100

    extracted code into component due to reusability reasons

---
+++ components/dns.vue
@@ -0,0 +1,60 @@
+<template>
+<div class="container mx-auto">
+  <div v-for="result in results" class="overflow-hidden bg-white rounded w-full shadow-md leading-normal mb-6 p-3">
+    <div v-if="result.domain" class="mb-4 text-3xl font-medium text-purple-700">
+      {{ result.domain }}
+    </div>
+    <div v-if="result.a_record" class="mb-4">
+      <strong>A redords</strong>
+      <ul class="text-xl text-gray-700 font-light">
+        <li v-for="a_record in result.a_record">
+          {{ a_record }}
+        </li>
+      </ul>
+    </div>
+    <div v-if="result.aaaa_record" class="mb-4">
+      <strong>AAAA redords</strong>
+      <ul class="text-xl text-gray-700 font-light">
+        <li v-for="aaaa_record in result.aaaa_record">
+          {{ aaaa_record }}
+        </li>
+      </ul>
+    </div>
+    <div v-if="result.cname_record" class="mb-4">
+      <strong>CNAME redords</strong>
+      <ul class="text-xl text-gray-700 font-light">
+        <li v-for="cname_record in result.cname_record">
+          {{ cname_record.target }}
+        </li>
+      </ul>
+    </div>
+    <div v-if="result.mx_record">
+      <strong>MX redords</strong>
+      <ul class="text-xl text-gray-700 font-light">
+        <li v-for="mx_record in result.mx_record">
+          {{ mx_record.exchange }}, {{ mx_record.preference }}
+        </li>
+      </ul>
+    </div>
+
+    <div v-if="result.header">
+      <strong>HTTP header</strong>
+      <code>
+        <ul class="bg-gray-300 overflow-scroll px-3 pt-3">
+          <li v-for="val, key in result.header">
+            <span class="font-bold">{{ key }}</span>: <span class="">{{ val }}</span>
+          </li>
+        </ul>
+      </code>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    results: Array
+  }
+}
+</script>
