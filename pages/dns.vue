9e7379f85efd8b947c51280309af079762647db6
Aurelius Wendelken <webtobesocial@gmail.com>
2019-12-03 03:46:20 +0100

    display http header for each record if available

--- pages/dns.vue
+++ pages/dns.vue
@@ -10,7 +10,7 @@
       <div v-if="result.a_record" class="mb-4">
         <strong>A redords</strong>
         <ul class="text-xl text-gray-700 font-light">
-          <li v-for="a_record in result.a_record" >
+          <li v-for="a_record in result.a_record">
             {{ a_record }}
           </li>
         </ul>
@@ -18,7 +18,7 @@
       <div v-if="result.aaaa_record" class="mb-4">
         <strong>AAAA redords</strong>
         <ul class="text-xl text-gray-700 font-light">
-          <li v-for="aaaa_record in result.aaaa_record" >
+          <li v-for="aaaa_record in result.aaaa_record">
             {{ aaaa_record }}
           </li>
         </ul>
@@ -26,7 +26,7 @@
       <div v-if="result.cname_record" class="mb-4">
         <strong>CNAME redords</strong>
         <ul class="text-xl text-gray-700 font-light">
-          <li v-for="cname_record in result.cname_record" >
+          <li v-for="cname_record in result.cname_record">
             {{ cname_record.target }}
           </li>
         </ul>
@@ -34,10 +34,21 @@
       <div v-if="result.mx_record">
         <strong>MX redords</strong>
         <ul class="text-xl text-gray-700 font-light">
-          <li v-for="mx_record in result.mx_record" >
+          <li v-for="mx_record in result.mx_record">
             {{ mx_record.exchange }}, {{ mx_record.preference }}
           </li>
         </ul>
+      </div>
+
+      <div v-if="result.header">
+        <strong>HTTP header</strong>
+        <code>
+          <ul class="bg-gray-300 overflow-scroll px-3 pt-3">
+            <li v-for="val, key in result.header">
+              <span class="font-bold">{{ key }}</span>: <span class="">{{ val }}</span>
+            </li>
+          </ul>
+        </code>
       </div>
     </div>
   </div>
