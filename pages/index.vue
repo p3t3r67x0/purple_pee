f343276a5f03f6baaf3ad8034c2fb82f077ff018
Aurelius Wendelken <webtobesocial@gmail.com>
2019-12-08 18:51:53 +0100

    adding information how to use filter queries for searching

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,68 @@
     </div>
   </div>
 
+  <div class="container mx-auto">
+    <div class="overflow-hidden bg-white rounded w-full shadow-md leading-normal mb-6 px-3 pt-2 pb-3">
+      <h2 class="text-3xl text-purple-800 font-light mb-2">Search with conditional Queries</h2>
+      <p class="w-9/12 text-xl mb-3">To get the most out of Purplepee it's important to understand the search query syntax. Search filters are special keywords to tell Purplepee that you wish to search specific properties. Note that there is no space in
+        between the filtername and its value. They take the format of...</p>
+      <ul class="text-lg">
+        <li class="mt-6">
+          <div class="bg-gray-200 p-2">
+            <code class="font-mono"><span class="text-orange-600">port:</span><span class="text-gray-700"></span>21</code>
+          </div>
+        </li>
+        <li class="mt-6">
+          <div class="bg-gray-200 p-2">
+            <code class="font-mono"><span class="text-orange-600">status:</span><span class="text-gray-700"></span>302</code>
+          </div>
+        </li>
+        <li class="mt-6">
+          <div class="bg-gray-200 p-2">
+            <code class="font-mono"><span class="text-orange-600">country:</span><span class="text-gray-700"></span>SG</code>
+          </div>
+          <p class="w-9/12 text-xl text-gray-700">
+            Using the above example, if you wanted to find devices located in Singapore then you would use the country search filter which accepts as an argument a 2-letter country code.
+          </p>
+        </li>
+        <li class="mt-6">
+          <div class="bg-gray-200 p-2">
+            <code class="font-mono"><span class="text-orange-600">org:</span><span class="text-gray-700"></span>hetzner</code>
+          </div>
+        </li>
+        <li class="mt-6">
+          <div class="bg-gray-200 p-2">
+            <code class="font-mono"><span class="text-orange-600">registry:</span><span class="text-gray-700"></span>ripencc</code>
+          </div>
+        </li>
+        <li class="mt-6">
+          <div class="bg-gray-200 p-2">
+            <code class="font-mono"><span class="text-orange-600">cidr:</span><span class="text-gray-700"></span>xxxx</code>
+          </div>
+        </li>
+        <li class="mt-6">
+          <div class="bg-gray-200 p-2">
+            <code class="font-mono"><span class="text-orange-600">server:</span><span class="text-gray-700"></span>nginx</code>
+          </div>
+        </li>
+        <li class="mt-6">
+          <div class="bg-gray-200 p-2">
+            <code class="font-mono"><span class="text-orange-600">site:</span><span class="text-gray-700"></span>example.org</code>
+          </div>
+        </li>
+        <li class="mt-6">
+          <div class="bg-gray-200 p-2">
+            <code class="font-mono"><span class="text-orange-600">cname:</span><span class="text-gray-700"></span>sunnysalan.com</code>
+          </div>
+        </li>
+        <li class="mt-6">
+          <div class="bg-gray-200 p-2">
+            <code class="font-mono"><span class="text-orange-600">mx:</span><span class="text-gray-700"></span>alt3.aspmx.l.google.com</code>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
   <dns v-if="results.length > 0 && results[0].domain != null" v-bind:results="results"></dns>
   <asn v-if="results.length > 0 && results[0].ip != null" v-bind:results="results"></asn>
 </div>
