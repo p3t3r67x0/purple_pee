9d89bcd52af8790d2de52065b56ab017b4936e7b
Aurelius Wendelken <webtobesocial@gmail.com>
2019-12-09 23:44:59 +0100

    show ports in results ther are any in the document

--- components/asn.vue
+++ components/asn.vue
@@ -5,34 +5,42 @@
       <p class="break-all">{{ result.name }}</p>
     </div>
     <div v-if="result.asn" class="mt-3 md:mt-4">
-      <strong>AS number</strong>
+      <strong class="text-lg">AS number</strong>
       <p class="text-lg md:text-xl text-gray-700 font-light">
         {{ result.asn }}
       </p>
     </div>
     <div v-if="result.ip" class="mt-3 md:mt-4">
-      <strong>IP address</strong>
+      <strong class="text-lg">IP address</strong>
       <p class="text-lg md:text-xl text-gray-700 font-light">
         {{ result.ip }}
       </p>
     </div>
     <div v-if="result.host" class="mt-3 md:mt-4">
-      <strong>Hostname</strong>
+      <strong class="text-lg">Hostname</strong>
       <p class="text-lg md:text-xl text-gray-700 font-light">
         {{ result.host }}
       </p>
     </div>
     <div v-if="result.cidr" class="mt-3 md:mt-4">
-      <strong>Prefixes</strong>
+      <strong class="text-lg">Prefixes</strong>
       <p class="text-lg md:text-xl text-gray-700 font-light">
         {{ result.cidr.join(', ') }}
       </p>
     </div>
     <div v-if="result.whois" class="mt-3 md:mt-4">
-      <strong>ASN whois</strong>
+      <strong class="text-lg">ASN whois</strong>
       <ul class="font-mono text-md font-light">
         <li v-for="v, k in result.whois" class="mt-1">
           <strong class="font-bold">{{ k }}</strong>:  <span class="text-gray-700 font-thin">{{ v }}</span>
+        </li>
+      </ul>
+    </div>
+    <div v-if="result.ports" class="mt-3 md:mt-4">
+      <strong class="text-lg">Ports</strong>
+      <ul class="font-mono text-md font-light">
+        <li v-for="port in result.ports" class="mt-1">
+          <strong class="font-bold">{{ port.port }}/{{ port.proto }}</strong>: <span>{{ port.status }}</span>
         </li>
       </ul>
     </div>
