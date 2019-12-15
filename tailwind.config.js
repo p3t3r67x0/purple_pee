73131490adb5b1b8659e996b5fd3922638a423c6
Aurelius Wendelken <webtobesocial@gmail.com>
2019-12-15 12:29:21 +0100

    restricted container max with to 1024px

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
 module.exports = {
-  theme: {},
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1024px',
+    }
+  },
   variants: {},
   plugins: []
 }
