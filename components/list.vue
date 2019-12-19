2f149af2ad976bd6663c1af9ed512c43164038c1
Aurelius Wendelken <webtobesocial@gmail.com>
2019-12-19 21:57:10 +0100

    updated cidr list view due to ui requirements

--- components/list.vue
+++ components/list.vue
@@ -1,15 +1,15 @@
 <template>
 <div class="container mx-auto">
-  <h1 class="text-xl font-thin mb-3">Latest added <strong class="font-bold">Subnets</strong></h1>
+  <h1 class="text-xl font-thin mx-3 md:mx-0 mb-3">Latest added <strong class="font-bold">Subnets</strong></h1>
 
-  <div class="flex flex-wrap justify-start overflow-hidden mx-3 md:mx-0 mb-6">
-    <ul v-for="result in results" class="mx-auto-3">
-      <li class="bg-white rounded shadow-md leading-normal p-3">
+  <ul class="flex content-start flex-wrap bg-gray-200 mb-6 mx-2 md:mx-0 p-1">
+    <li v-for="result in results" class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-1">
+      <div v-if="result.whois" class="bg-white rounded shadow-md leading-normal p-3">
         <strong class="block">AS {{ result.whois.asn }}</strong>
         <nuxt-link v-bind:to="generateLink(result.whois.asn_cidr)" class="md:text-xl font-light text-blue-500 hover:text-blue-700">{{ result.whois.asn_cidr }}</nuxt-link>
-      </li>
-    </ul>
-  </div>
+      </div>
+    </li>
+  </ul>
 </div>
 </template>
 
