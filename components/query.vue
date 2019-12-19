3309e5f6bc664507320349ada0b1a56b720b05c9
Aurelius Wendelken <webtobesocial@gmail.com>
2019-12-19 23:05:29 +0100

    added space around the query title

--- components/query.vue
+++ components/query.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="container mx-auto mb-3">
+<div class="container mx-auto mx-3 md:mx-0 mb-3">
   <h1 class="text-xl font-thin">Results for <strong class="font-bold">{{ query }}</strong></h1>
 </div>
 </template>
