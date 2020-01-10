56ef0b675575eac4043e23487b27569fd5ed6e4b
Aurelius Wendelken <webtobesocial@gmail.com>
2020-01-10 23:11:23 +0100

    added a title to each grid of trends to improve UX readability

--- components/trends-list.vue
+++ components/trends-list.vue
@@ -5,6 +5,7 @@
   <ul class="flex content-start flex-wrap bg-gray-200 mb-6 mx-2 md:mx-0 p-1">
     <li v-for="result in results" class="w-full sm:w-1/2 md:w-1/3 p-1">
       <div class="bg-white rounded shadow-md leading-normal p-3">
+        <p class="font-bold font-mono">{{ generateTitle(generateLink(result.trend)) }}</p>
         <span><img v-if="isCountry(result.trend)" v-bind:src="generatePath(generateName(result.trend))" class="inline w-9 h-4 mr-1"></span>
         <nuxt-link v-bind:to="generateLink(result.trend)" class="font-mono font-light text-base text-blue-500 hover:text-blue-700">{{ generateName(result.trend) }}</nuxt-link>
       </div>
@@ -45,6 +46,47 @@ export default {
     generateName(path) {
       const r = path.split(':').filter(Boolean)
       return r.splice(-1)[0]
+    },
+    generateTitle(path) {
+      const r = path.split('/').filter(Boolean)
+
+      if (r[0] === 'mx') {
+        return 'MX record'
+      } else if (r[0] === 'ipv4') {
+        return 'A record'
+      } else if (r[0] === 'ipv6') {
+        return 'AAAA record'
+      } else if (r[0] === 'cname') {
+        return 'CNAME record'
+      } else if (r[0] === 'soa') {
+        return 'SOA record'
+      } else if (r[0] === 'ns') {
+        return 'NS record'
+      } else if (r[0] === 'cidr') {
+        return 'CIDR'
+      } else if (r[0] === 'dns') {
+        return 'DNS'
+      } else if (r[0] === 'asn') {
+        return 'ASN'
+      } else if (r[0] === 'org') {
+        return 'ISP'
+      } else if (r[0] === 'registry') {
+        return 'Registry'
+      } else if (r[0] === 'country') {
+        return 'ISO Code'
+      } else if (r[0] === 'status') {
+        return 'HTTP Status'
+      } else if (r[0] === 'port') {
+        return 'TCP Port'
+      } else if (r[0] === 'server') {
+        return 'Server'
+      } else if (r[0] === 'site') {
+        return 'Domain'
+      } else if (r[0] === 'ssl') {
+        return 'SSL'
+      } else if (r[0] === 'app') {
+        return 'Service'
+      }
     },
     generateLink(path) {
       const r = path.split(':').filter(Boolean)
