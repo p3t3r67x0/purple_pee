e4c5e61fee6d1f68274c6a99e1f5cf1c556f9e85
Aurelius Wendelken <webtobesocial@gmail.com>
2020-01-13 00:12:24 +0100

    added new routes and methods for ssl certificate modules

---
+++ pages/ssl/_.vue
@@ -0,0 +1,83 @@
+<template>
+<div class="min-h-screen flex flex-col">
+  <div class="flex-grow">
+    <navheader></navheader>
+    <modal v-if="modalVisible"></modal>
+    <query v-if="!loadingIndicator" v-bind:query="query" v-bind:results="results"></query>
+    <dns v-if="!loadingIndicator" v-bind:results="results"></dns>
+  </div>
+  <navfooter></navfooter>
+</div>
+</template>
+
+<script>
+import Dns from '@/components/dns.vue'
+import Modal from '@/components/modal.vue'
+import Query from '@/components/query.vue'
+import Footer from '@/components/navfooter.vue'
+import Navbar from '@/components/navheader.vue'
+
+export default {
+  components: {
+    dns: Dns,
+    query: Query,
+    modal: Modal,
+    navfooter: Footer,
+    navheader: Navbar
+  },
+  data() {
+    return {
+      results: [],
+    }
+  },
+  created() {
+    this.fetchLatest(this.query)
+    this.$store.commit('updateQuery', 'ssl:' + this.query)
+  },
+  computed: {
+    modalVisible() {
+      return this.$store.state.modalVisible
+    },
+    loadingIndicator() {
+      return this.$store.state.loading
+    },
+    query() {
+      return this.$route.params.pathMatch
+    }
+  },
+  head() {
+    return {
+      title: 'SSL certificate results for ' + this.query,
+      meta: [{
+        hid: 'description',
+        name: 'description',
+        content: 'Explore latest ssl certificate results ' + this.query
+      }]
+    }
+  },
+  methods: {
+    fetchLatest(query) {
+      this.$axios.$get(process.env.API_URL + '/match/ssl:' + query).then(res => {
+        this.results = res
+      }).catch((error) => {
+        if (error.response) {
+          this.$store.commit('updateResultList', [])
+          this.$router.push({
+            name: 'search-all',
+            params: {
+              pathMatch: query
+            }
+          })
+
+          if (error.response.status !== 404) {
+            this.$store.commit('updateErrorMessage', error.response.data)
+            this.$store.commit('updateErrorStatus', error.response.status)
+            this.$store.commit('updateModalVisible', true)
+            this.$store.commit('updateLoadingIndicator', false)
+          }
+        }
+      })
+    }
+  }
+}
+</script>
